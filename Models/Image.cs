be5a6e3a4c13c27e72529aba12d7ce9f45959dc4
sigurdstuge <sigurd.stuge@gmail.com>
2025-05-21 11:36:26 +0200

    Implemented Models Image file

---
+++ Models/Image.cs
@@ -0,0 +1,10 @@
+using ImageApi.Interfaces;
+
+namespace ImageApi.Models;
+
+public class Image : IImage
+{
+	public int Id {get; set;}
+	public string Name {get; set;}
+	public string Img {get; set;}
+}
